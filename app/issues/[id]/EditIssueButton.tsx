c1a12cf7fbeed87002d6ce78cbc5a993e60f31cd
Hamza <hamzachaudhay79@gmail.com>
2024-04-17 15:14:27 +0500

    Refactor : Issue Details page

---
+++ app/issues/[id]/EditIssueButton.tsx
@@ -0,0 +1,16 @@
+import { Issue } from "@prisma/client";
+import { Pencil2Icon } from "@radix-ui/react-icons";
+import { Button } from "@radix-ui/themes";
+import Link from "next/link";
+import React from "react";
+
+const EditIssueButton = ({ issue }: { issue: Issue }) => {
+  return (
+    <Button>
+      <Pencil2Icon />
+      <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
+    </Button>
+  );
+};
+
+export default EditIssueButton;
