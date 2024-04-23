1c21ccf9487eb2fe152e7bf7d9fd44805902beaf
Hamza <hamzachaudhay79@gmail.com>
2024-04-23 22:16:04 +0500

    changing bigint to number

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -60,7 +60,7 @@ const IssuesPage = async ({ searchParams }: Props) => {
         </Table.Header>
         <Table.Body>
           {issues.map((issue) => (
-            <Table.Row key={issue.id}>
+            <Table.Row key={Number(issue.id)}>
               <Table.Cell>
                 <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
                 <div className="block md:hidden">
